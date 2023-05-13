210e5946fb5247074ee34e55912e11e956114c75
daitc2209 <trandai1116@gmail.com>
2023-05-13 11:02:35 +0700

    done

--- employee/src/main/java/com/example/employee/service/JwtAuthEntryPoint.java
+++ employee/src/main/java/com/example/employee/service/JwtAuthEntryPoint.java
@@ -1,19 +1,32 @@
 package com.example.employee.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, authException.getMessage());
+//        response.sendError(HttpServletResponse.SC_FORBIDDEN, authException.getMessage());
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("status", "403");
+        errorResponse.put("error", "Forbidden");
+        errorResponse.put("message", "Access Denied");
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(new ObjectMapper().writeValueAsString(errorResponse));
     }
 }
